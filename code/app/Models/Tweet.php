7531ae3a8434353dfcf66349a62af00cbf9c6f7a
EmptySpace99 <chirigoni05@hotmail.it>
2021-06-25 00:44:10 +0200

    clear model

--- code/app/Models/Tweet.php
+++ code/app/Models/Tweet.php
@@ -4,10 +4,5 @@ require_once(__DIR__ . '/../../libs/Model/Model.php');
 
 class Tweet extends Model{
 
-    //The fillable property is used to represent mass assignable attributes for our model
-    protected $fillable = [
-        'content',
-    ];
-
     
 }
