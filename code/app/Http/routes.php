81110c6d7d3d3e4b8304ed0fbf546446e4dde222
EmptySpace99 <chirigoni05@hotmail.it>
2021-06-25 00:42:29 +0200

    change routes

--- code/app/Http/routes.php
+++ code/app/Http/routes.php
@@ -6,13 +6,13 @@ Router::get('/',['use'=>'HomeController@home']);
 # 2) bug "PHP message: PHP Notice:  Undefined offset: 1 in /var/www/html/code/libs/Route/Route.php on line 103"
 
 Router::get('/tweets',['use'=>'TweetController@showTweets']);
-Router::get('/tweet/create',['use'=>'TweetController@createTweet']);
+Router::get('/tweet',['use'=>'TweetController@createTweet']);
 
 /*
 * Tweet API
 */
-Router::get('/api/tweets',['use'=>'TweetController@allTweets']);
-Router::post('/api/tweet/create',['use'=>'TweetController@storeTweet']);
-Router::get('/api/tweet/read/{id}',['use'=>'TweetController@readTweet']);
-Router::patch('/api/tweet/update/{id}',['use'=>'TweetController@updateTweet']);
-Router::delete('/api/tweet/delete/{id}',['use'=>'TweetController@deleteTweet']);
+Router::get('/api/tweet',['use'=>'TweetController@allTweets']);
+Router::post('/api/tweet',['use'=>'TweetController@storeTweet']);
+Router::get('/api/tweet/{id}',['use'=>'TweetController@readTweet']);
+Router::patch('/api/tweet/{id}',['use'=>'TweetController@updateTweet']);
+Router::delete('/api/tweet/{id}',['use'=>'TweetController@deleteTweet']);
